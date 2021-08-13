977c550bbc778c015d9443a745560629ff8ca78c
karan100010 <karanjoshi1993@gmail.com>
2021-08-13 14:01:03 +0530

    changed logs location to radio.py

--- radio.py
+++ radio.py
@@ -42,7 +42,28 @@ coloredlogs.install()
 
 # *** Global Variables *** #
 
+coloredlogs.install()
+logging.basicConfig(filename="logfile.log",
+                            filemode='a',
+                            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                            datefmt='%H:%M:%S',
+                            level=logging.INFO)
+# create logger
+logger = logging.getLogger('simple_example')
+logger.setLevel(logging.DEBUG)
 
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)        
 
 
 #def wavFilesJoin(file1,file):
