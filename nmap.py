75b849f50efaeb7053688f9de66ceda14b628b1e
karan100010 <karanjoshi1993@gmail.com>
2021-09-17 22:18:05 +0530

    added nmap3

--- nmap.py
+++ nmap.py
@@ -1,13 +1,12 @@
 
-import os
-os.chdir("/home/pi/")
-import nmap
 from time import sleep
+import nmap3
 
 def get_lan_ip():
-    nm=nmap.PortScanner()
-    nm.scan('192.168.1.1/24','22')
-    return nm.all_hosts()
+       
+        nmap = nmap3.Nmap()
+        results = nmap.scan_top_ports("192.168.1.1/24", args="-sn")
+        return [i for i in results.keys() if "192.168" in i]
 ip_list=["192.168.1.41","192.168.1.42","192.168.1.43","192.168.1.44","192.168.1.45"]
 with open('ip_list.txt', 'w') as f:
     for item in ip_list:
