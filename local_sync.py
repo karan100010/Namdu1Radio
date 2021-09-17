0025a838086d62aec3797e309615b9e06a914470
karan100010 <karanjoshi1993@gmail.com>
2021-09-17 22:32:04 +0530

    added

--- local_sync.py
+++ local_sync.py
@@ -8,12 +8,12 @@ def sync(catname,ipmap,logger):
         try:
             Popen("sshpass -f 'sshpass' rsync -azP /var/www/html/.upload/"+catname+" pi@"+i+":/var/www/html/.upload/"+catname+" --ignore-existing")
         except Exception as e:
-            logger("following exception occred "+e+" while tranferring to "+i) 
+            logger("following exception occred "+str(e)+" while tranferring to "+i) 
 
         try:
             Popen("sshpass -f 'sshpass' rsync -azP pi@"+i+":/var/www/html/.upload/"+catname+"/ /var/www/html/.upload/"+catname+" --ignore-existing")
         except Exception as e:
-            logger("following exception occred "+e+" while tranferring to "+i)     
+            logger("following exception occred "+str(e)+" while tranferring to "+i)     
 
 
 def sync_background(catname,ipmap,logger):
