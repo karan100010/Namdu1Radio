0df52006f24c57c4149c38e4d8c5562cb1578c13
karan100010 <karanjoshi1993@gmail.com>
2021-09-17 22:53:47 +0530

    added

--- local_sync.py
+++ local_sync.py
@@ -1,17 +1,18 @@
 
 from subprocess import Popen
 from multiprocessing import Process
+import os
 
     
 def sync(catname,ipmap,logger):
     for i in ipmap:
         try:
-            Popen("sshpass -f 'sshpass' rsync -azP /var/www/html/.upload/"+catname+" pi@"+i+":/var/www/html/.upload/"+catname+" --ignore-existing")
+            os.system("sshpass -f 'sshpass' rsync -azP /var/www/html/.upload/"+catname+" pi@"+i+":/var/www/html/.upload/"+catname+" --ignore-existing")
         except Exception as e:
             logger.info("error occerd while pushing to  "+i) 
 
         try:
-            Popen("sshpass -f 'sshpass' rsync -azP pi@"+i+":/var/www/html/.upload/"+catname+"/ /var/www/html/.upload/"+catname+" --ignore-existing")
+            os.system("sshpass -f 'sshpass' rsync -azP pi@"+i+":/var/www/html/.upload/"+catname+"/ /var/www/html/.upload/"+catname+" --ignore-existing")
         except Exception as e:
             logger.info("error occerd while pushing to  "+i)    
 
