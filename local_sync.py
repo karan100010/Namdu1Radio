e1d1875cc1862d2fb591e88b6d463e6c25cad624
karan100010 <karanjoshi1993@gmail.com>
2021-09-18 11:43:05 +0530

    added syncing

--- local_sync.py
+++ local_sync.py
@@ -7,7 +7,7 @@ import os
 def sync(catname,ipmap,logger):
     for i in ipmap:
         try:
-            os.system("sshpass -f 'sshpass' rsync -azP /var/www/html/.upload/"+catname+" pi@"+i+":/var/www/html/.upload/"+catname+" --ignore-existing")
+            os.system("sshpass -f 'sshpass' rsync -azP /var/www/html/.upload/"+catname+"/ pi@"+i+":/var/www/html/.upload/"+catname+" --ignore-existing")
         except Exception as e:
             logger.info("error occerd while pushing to  "+i) 
 
