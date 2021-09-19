17de376e889893e52388de6dc1c5da9898b59a24
karan100010 <karanjoshi1993@gmail.com>
2021-09-19 18:24:46 +0530

    changed path for sshfile

--- local_sync.py
+++ local_sync.py
@@ -7,12 +7,12 @@ import os
 def sync(catname,ipmap,logger):
     for i in ipmap:
         try:
-            os.system("sshpass -f 'sshpass' rsync -azP /var/www/html/.upload/"+catname+"/ pi@"+i+":/var/www/html/.upload/"+catname+" --ignore-existing")
+            os.system("sshpass -f '/home/pi/Documents/Namdu1radio/sshpass' rsync -azP /var/www/html/.upload/"+catname+"/ pi@"+i+":/var/www/html/.upload/"+catname+" --ignore-existing")
         except Exception as e:
             logger.info("error occerd while pushing to  "+i) 
 
         try:
-            os.system("sshpass -f 'sshpass' rsync -azP pi@"+i+":/var/www/html/.upload/"+catname+"/ /var/www/html/.upload/"+catname+" --ignore-existing")
+            os.system("sshpass -f '/home/pi/Documents/Namdu1radio/sshpass' rsync -azP pi@"+i+":/var/www/html/.upload/"+catname+"/ /var/www/html/.upload/"+catname+" --ignore-existing")
         except Exception as e:
             logger.info("error occerd while pushing to  "+i)    
 
