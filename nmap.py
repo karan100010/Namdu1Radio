40fa4bb32b85cbf49381883dd496ded3273abb42
karan100010 <karanjoshi1993@gmail.com>
2021-10-05 17:42:00 +0530

    added to nmap

--- nmap.py
+++ nmap.py
@@ -12,9 +12,9 @@ ip_list=["192.168.1.41","192.168.1.42","192.168.1.43","192.168.1.44","192.168.1.
 with open('ip_list.txt', 'w') as f:
     for item in ip_list:
         f.write("%s\n" % item)
-while True:
-    ip_list=get_lan_ip()
-    with open('ip_list.txt', 'w') as f:
-        for item in ip_list:
-            f.write("%s\n" % item)
-    sleep(30)
+
+ip_list=get_lan_ip()
+with open('ip_list.txt', 'w') as f:
+    for item in ip_list:
+        f.write("%s\n" % item)
+
