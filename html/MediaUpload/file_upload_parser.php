da8c97eb1f924ff62c9d277f336741b583aaf2f2
karan100010 <karanjoshi1993@gmail.com>
2021-10-18 20:30:36 +0530

    changed download file

--- html/MediaUpload/file_upload_parser.php
+++ html/MediaUpload/file_upload_parser.php
@@ -1,6 +1,6 @@
 <?php
 
-$fileName = $_POST["finalname"]; // The file name
+$fileName = $_FILES["finalname"]; // The file name
 $fileTmpLoc = $_FILES["file1"]["tmp_name"]; // File in the PHP tmp folder
 $fileType = $_FILES["file1"]["type"]; // The type of file it is
 $fileSize = $_FILES["file1"]["size"]; // File size in bytes
